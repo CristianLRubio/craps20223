c9ffcd0ae7fbedf5ae7751e01dc185b183a289f7
CristianLRubio <cleonardor@correo.udistrital.edu.co>
2022-09-14 17:11:10 -0500

    v0.0.2

--- sandbox.py
+++ sandbox.py
@@ -1,15 +1,5 @@
+from juego import Juego
 
+miJuego=Juego()
 
-from Dado import Dado
-
-
-dado_1=Dado()
-dado_2=Dado()
-
-
-dado_1.girar()
-dado_2.girar()
-
-juego= dado_1.getValor()+dado_2.getValor()
-
-print(juego)
+miJuego.lanzar_punto()
